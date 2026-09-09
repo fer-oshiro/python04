f0c771c97e11cbdebf3ae8248ed1677c902bfda4
Fernanda Oshiro <fsayuri-@student.42sp.org.br>
2026-09-09 13:22:25 -0300

    ex0

--- ex0/ft_ancient_text.py
+++ ex0/ft_ancient_text.py
@@ -3,25 +3,27 @@ import sys
 
 
 def main() -> None:
-    if len(sys.argv) == 1:
+    if len(sys.argv) != 2:
         print("Usage: ft_ancient_text.py <file>")
         return
 
+    filename = sys.argv[1]
     print("=== Cyber Archives Recovery ===")
+    print(f"Accessing file '{filename}'")
+
+    file = None
     try:
-        print(f"Accessing file '{sys.argv[1]}'")
-        file = open(sys.argv[1], 'r')
+        file = open(filename, "r")
+        data = file.read()
         print("---", end="\n\n")
-        for line in file:
-            print(line, end="")
-        print("\n\n---")
-        file.close()
-        if file.closed:
-            print(f"File '{sys.argv[1]}' closed.")
-    except FileNotFoundError as e:
-        print(f"Error opening file '{sys.argv[1]}': {e}")
-    except PermissionError as e:
-        print(f"Error opening file '{sys.argv[1]}': {e}")
+        print(data)
+        print("\n---")
+    except (OSError, UnicodeDecodeError) as e:
+        print(f"Error opening file '{filename}': {e}")
+    finally:
+        if file is not None:
+            file.close()
+            print(f"File '{filename}' closed.")
 
 
 if __name__ == "__main__":
